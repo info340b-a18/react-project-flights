646d412e3ffbde61a8529e17afdb0d244ccf80c6
jenli36 <lcy99@uw.edu>
2018-11-20 11:56:42 -0800

    router

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Homepage } from './Homepage.js';
+import { Airlines } from './Airlines.js';
+import { Region } from './Region.js';
+import { About } from './About.js';
+
+export class App extends Component {
+  render() {
+    
+    return (
+      <div className="container">
+      <h1>Flights</h1>
+        <Router>
+      <div className="contianer">
+      <Link to="/">Home</Link>-
+      <Link to="/Airlines">Airline</Link>-
+      <Link to="/Region">Region</Link>
+      <Link to ="/About">About</Link>
+      <Route exact path="/" component={Homepage} />
+      <Route path="/Airlines" component={Airlines} />
+      <Route path="/Region" component={Region} />
+      <Route path = "/about" component={About} />
+      </div>
+    </Router>
+      </div>
+    )
+  }
+}
