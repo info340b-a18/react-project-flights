e7936b7328212f553a99ba72247c0dad86c9f45a
James Kim <jameskim.1997@yahoo.com>
2018-11-21 20:32:13 -0800

    a

--- src/App.js
+++ src/App.js
@@ -8,22 +8,21 @@ import { About } from './About.js';
 
 export class App extends Component {
   render() {
-    
     return (
       <div className="container">
-      <h1>Flights</h1>
+        <h1>Flights</h1>
         <Router>
-      <div className="contianer">
-      <Link to="/">Home</Link>-
+          <div className="contianer">
+            <Link to="/">Home</Link>-
       <Link to="/Airlines">Airline</Link>-
       <Link to="/Region">Region</Link>
-      <Link to ="/About">About</Link>
-      <Route exact path="/" component={Homepage} />
-      <Route path="/Airlines" component={Airlines} />
-      <Route path="/Region" component={Region} />
-      <Route path = "/about" component={About} />
-      </div>
-    </Router>
+            <Link to="/About">About</Link>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/Airlines" component={Airlines} />
+            <Route path="/Region" component={Region} />
+            <Route path="/about" component={About} />
+          </div>
+        </Router>
       </div>
     )
   }
