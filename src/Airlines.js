43af29c17564388430b8009eceb4244966454c0a
jenli36 <lcy99@uw.edu>
2018-11-27 11:43:30 -0800

    linechart

--- src/Airlines.js
+++ src/Airlines.js
@@ -5,8 +5,9 @@ import { Card, CardImg, CardText, CardBody,
 import chart1 from './img/basic-airlines.png'
 import chart2 from './img/airlines-graph1.png'
 import chart3 from './img/airlines-graph2.png'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-    export class Airlines extends Component {
+    /*export class Airlines extends Component {
         render() {
             return(
             <div>
@@ -34,5 +35,33 @@ import chart3 from './img/airlines-graph2.png'
                 </CardGroup>
              </div>
             );
+        }
+    }*/
+    const data = [
+        {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+        {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+        {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+        {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+        {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+        {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+        {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+  ];
+    export class Airlines extends Component {
+        render() {
+            
+        return(
+        <LineChart
+  width={400}
+  height={400}
+  data={data}
+  margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+>
+  <XAxis dataKey="name" />
+  <Tooltip />
+  <CartesianGrid stroke="#f5f5f5" />
+  <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+  <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+</LineChart>
+        )
         }
     }
