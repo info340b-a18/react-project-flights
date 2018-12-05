c454b302302b1baed24bb759a645dec92c912770
Wonjo Barng <wjbarng@uw.edu>
2018-12-05 15:33:32 -0800

    merge

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 
 
 import firebase from 'firebase/app';
-<<<<<<< HEAD
 import 'firebase/auth'; 
 import 'firebase/database';
 
@@ -18,19 +17,5 @@ import 'firebase/database';
     messagingSenderId: "408356926270"
   };
   firebase.initializeApp(config);
-=======
-import 'firebase/auth';
-
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDZNudFV3Fm7hBYoI8-Y83-1BA78-1egfY",
-    authDomain: "react-flights-5d8ac.firebaseapp.com",
-    databaseURL: "https://react-flights-5d8ac.firebaseio.com",
-    projectId: "react-flights-5d8ac",
-    storageBucket: "react-flights-5d8ac.appspot.com",
-    messagingSenderId: "127604908613"
-};
-firebase.initializeApp(config);
->>>>>>> e7ebe787e5068122446d7dccab99c2323915ffc1
 
 ReactDOM.render(<App />, document.getElementById('root'));
