75eec9c3b8f888df3fcbdd607340873e35b8a189
Wonjo Barng <wjbarng@uw.edu>
2018-12-05 19:30:05 -0800

    changed dropdown menu

--- src/login.js
+++ src/login.js
@@ -104,7 +104,13 @@ export class Login extends Component {
             
             <div className="form-group">
             <label htmlFor="password">Password</label>
-            <p>{this.state.user.password}</p>
+            <input className="form-control" 
+                id="password" 
+                type="password"
+                name="password"
+                placeholder="type here to change the password"
+                onChange={(e) => this.handleChange(e)}
+            />
             </div>
     
             <div className="form-group">
@@ -120,34 +126,63 @@ export class Login extends Component {
     
             <div className="form-group">
             <label htmlFor="airline">Airline</label>
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                <DropdownToggle caret>
+            {console.log(this.state.user)}
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} style={{'background-color': '#003459', 'color': 'white'}}>
+                <DropdownToggle className="col-12" caret style={{'background-color': '#003459', 'color': 'white'}}>
+                {this.state.user.airline === "all"?
+                    "Show All"
+                :this.state.user.airline === "UA"?
+                    "United Airlines"
+                :this.state.user.airline === "AA"?
+                    "American Airlines"
+                :this.state.user.airline === "US"?
+                    "US Airways"
+                :this.state.user.airline === "F9"?
+                    "Frontier Airlines"
+                :this.state.user.airline === "B6"?
+                    "JetBlue Airways"
+                :this.state.user.airline === "OO"?
+                    "Skywest Airlines"
+                :this.state.user.airline === "AS"?
+                    "Alaska Airlines"
+                :this.state.user.airline === "WN"?
+                    "Spirit Air Lines"
+                :this.state.user.airline === "DL"?
+                    "Southwest Airlines"
+                :this.state.user.airline === "EV"?
+                    "Atlantic Southeast Airlines"
+                :this.state.user.airline === "HA"?
+                    "Hawaiian Airlines"
+                :this.state.user.airline === "MQ"?
+                    "American Eagle Airlines"
+                : "Virgin America"
+                }
                 {this.state.user.airline}
                 </DropdownToggle>
-                <DropdownMenu>
-                <DropdownItem value="all" onClick={(e) => this.setState({airlinename: "Show All"})}>Show All</DropdownItem >
-                    <DropdownItem value="UA" onClick={(e) => this.setState({airlinename: "United Airlines"})}>United Airlines</DropdownItem >
-                    <DropdownItem value="AA" onClick={(e) => this.setState({airlinename: "American Airlines"})}>American Airlines</DropdownItem>
-                    <DropdownItem value="US" onClick={(e) => this.setState({airlinename: "US Airways"})}>US Airways</DropdownItem>
-                    <DropdownItem value="F9" onClick={(e) => this.setState({airlinename: "Frontier Airlines"})}>Frontier Airlines</DropdownItem>
-                    <DropdownItem value="B6" onClick={(e) => this.setState({airlinename: "JetBlue Airways"})}>JetBlue Airways</DropdownItem>
-                    <DropdownItem value="OO" onClick={(e) => this.setState({airlinename: "Skywest Airlines"})}>Skywest Airlines</DropdownItem>
-                    <DropdownItem value="AS" onClick={(e) => this.setState({airlinename: "Alaska Airlines"})}>Alaska Airlines</DropdownItem>
-                    <DropdownItem value="WN" onClick={(e) => this.setState({airlinename: "Spirit Air Lines"})}>Spirit Air Lines</DropdownItem>
-                    <DropdownItem value="DL" onClick={(e) => this.setState({airlinename: "Southwest Airlines"})}>Southwest Airlines</DropdownItem>
-                    <DropdownItem value="EV" onClick={(e) => this.setState({airlinename: "Atlantic Southeast Airlines"})}>Atlantic Southeast Airlines</DropdownItem>
-                    <DropdownItem value="HA" onClick={(e) => this.setState({airlinename: "Hawaiian Airlines"})}>Hawaiian Airlines</DropdownItem>
-                    <DropdownItem value="MQ" onClick={(e) => this.setState({airlinename: "American Eagle Airlines"})}>American Eagle Airlines</DropdownItem>
-                    <DropdownItem value="VX" onClick={(e) => this.setState({airlinename: "Virgin America"})}>Virgin America</DropdownItem>
+                <DropdownMenu className="col-12" style={{'background-color': '#003459', 'color': 'white'}}>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="all" onClick={(e) => this.setState({airlinename: "Show All"})}>Show All</DropdownItem >
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="UA" onClick={(e) => this.setState({airlinename: "United Airlines"})}>United Airlines</DropdownItem >
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="AA" onClick={(e) => this.setState({airlinename: "American Airlines"})}>American Airlines</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="US" onClick={(e) => this.setState({airlinename: "US Airways"})}>US Airways</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="F9" onClick={(e) => this.setState({airlinename: "Frontier Airlines"})}>Frontier Airlines</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="B6" onClick={(e) => this.setState({airlinename: "JetBlue Airways"})}>JetBlue Airways</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="OO" onClick={(e) => this.setState({airlinename: "Skywest Airlines"})}>Skywest Airlines</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="AS" onClick={(e) => this.setState({airlinename: "Alaska Airlines"})}>Alaska Airlines</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="WN" onClick={(e) => this.setState({airlinename: "Spirit Air Lines"})}>Spirit Air Lines</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="DL" onClick={(e) => this.setState({airlinename: "Southwest Airlines"})}>Southwest Airlines</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="EV" onClick={(e) => this.setState({airlinename: "Atlantic Southeast Airlines"})}>Atlantic Southeast Airlines</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="HA" onClick={(e) => this.setState({airlinename: "Hawaiian Airlines"})}>Hawaiian Airlines</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="MQ" onClick={(e) => this.setState({airlinename: "American Eagle Airlines"})}>American Eagle Airlines</DropdownItem>
+                    <DropdownItem className="col-12" style={{'background-color': '#003459', 'color': 'white'}} value="VX" onClick={(e) => this.setState({airlinename: "Virgin America"})}>Virgin America</DropdownItem>
                 </DropdownMenu>
             </Dropdown>
             </div>
 
         </form>
                 {this.state.user &&
-                    <button className="logout btn btn-warning" 
+                    <button className="logout btn" style={{'background-color': '#003459', 'color': 'white'}} 
                             onClick={() => this.handleSignOut()}>
-                    Log Out {this.state.user.displayName}
+                    Log Out
                     </button>
                 }
             </div>
