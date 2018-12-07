d1bf474d4ce27f8a0b2e5e41a310e9aba7d14bcb
James Kim <jameskim.1997@yahoo.com>
2018-12-07 15:39:54 -0800

    more style changes

--- src/About.js
+++ src/About.js
@@ -20,18 +20,19 @@ export class About extends Component {
                             <CardText>Flight! is an interactive application to display the best airline to fly with
                             when flying domestically in the United States. You can create an account with your preferred Airline and see how it compares with other airlines.
                             It compares flight delays in United States. This website is made possible thanks to
-                     <a href="https://www.kaggle.com/fabiendaniel/predicting-flight-delays-tutorial/notebook" target="_blank" rel="noopener noreferrer" className="link"> Kaggle's Dataset</a>.<br></br>
+                     <a href="https://www.kaggle.com/fabiendaniel/predicting-flight-delays-tutorial/notebook" target="_blank" rel="noopener noreferrer" className="link"> Kaggle's Dataset</a>.<br></br><br></br>
                                 Air travel in 2017 was miserable. With the combination of "more flights,
                                     social media, rising fees, and diminishing personal space," led to regular
                     air-passenger outrage(<a href="https://tinyurl.com/y72bbdd2" target="_blank" rel="noopener noreferrer" className="link">Washington Post</a>).
                                 Whether it is on the extreme side with flight attendants dragging passengers off the
                                 plane or simple annoyances such as flight delays, there seem to be a wide range of
-                    tedium with flying.<br></br>
+                    tedium with flying.<br></br><br></br>
                                 Even though air travel is annoying, it is unavoidable for many of us. With this application,
                                     you can explore which airlines and airports passengers can fly with to make for
                                     a better experience. In perhaps reducing these annoyances, passengers could
                                     potentially make informed decisions for investing their money with the better
-                                    airline to continue their loyalty to earn frequent flyer miles for rewards.
+                                    airline to continue their loyalty to earn frequent flyer miles for rewards.<br></br><br></br>
+                                This was made by James Kim, Jennifer Li, Won Barng, Leo Kwo
                         </CardText>
                         </CardBody>
                     </div>
--- src/Airlines.js
+++ src/Airlines.js
@@ -102,7 +102,9 @@ export class Airlines extends Component {
       <div>
         <Card>
           <CardBody>
-            <div className="box" id="graph">
+            <div className="box" id="graph" style={{
+              paddingTop: '3.5rem'
+            }}>
               <CardBody>
                 <CardTitle>Airline Delay Time</CardTitle>
                 <CardText>This graph demonstrates the maximum and minimum delay time of each airline.</CardText>
@@ -136,7 +138,9 @@ export class Airlines extends Component {
                 </div>
               </CardBody>
             </div>
-            <div className="box" id="table">
+            <div className="box" id="table" style={{
+              paddingTop: '3.5rem'
+            }}>
               <CardBody>
                 <CardTitle>Airline Information</CardTitle>
 
