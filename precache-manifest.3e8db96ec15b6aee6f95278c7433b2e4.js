c4da713d5238366464f29b4ea1e89297d06e66d3
James Kim <jameskim.1997@yahoo.com>
2018-12-07 17:09:25 -0800

    Updates

---
+++ precache-manifest.3e8db96ec15b6aee6f95278c7433b2e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaee426df76ed313863532de8911eaaa",
+    "url": "/react-project-flights/static/media/region.aaee426d.jpg"
+  },
+  {
+    "revision": "fac2d39dd9c638cbc4e1ddf11d109f88",
+    "url": "/react-project-flights/static/media/airport.fac2d39d.jpeg"
+  },
+  {
+    "revision": "4a86379a0bd7565a35d88865092eaab3",
+    "url": "/react-project-flights/static/media/airline.4a86379a.jpg"
+  },
+  {
+    "revision": "e474bf3a79899f35bdd9",
+    "url": "/react-project-flights/static/js/runtime~main.e474bf3a.js"
+  },
+  {
+    "revision": "ecbbbac6ff077314c7d9",
+    "url": "/react-project-flights/static/js/main.ecbbbac6.chunk.js"
+  },
+  {
+    "revision": "19222002fdd0a4047985",
+    "url": "/react-project-flights/static/js/1.19222002.chunk.js"
+  },
+  {
+    "revision": "ecbbbac6ff077314c7d9",
+    "url": "/react-project-flights/static/css/main.20daa728.chunk.css"
+  },
+  {
+    "revision": "19222002fdd0a4047985",
+    "url": "/react-project-flights/static/css/1.99a5d385.chunk.css"
+  },
+  {
+    "revision": "ed1c6eebcbf32d99698a047d5a113d33",
+    "url": "/react-project-flights/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-project-flights/precache-manifest.a1d6f91e0c2809c481e020abc9ca6cfd.js"
+  "/react-project-flights/precache-manifest.3e8db96ec15b6aee6f95278c7433b2e4.js"
 );
 
 workbox.clientsClaim();
